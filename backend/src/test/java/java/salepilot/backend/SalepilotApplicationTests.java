6def1020c0d0cecd21c6fc1c4687e4a963f20fcd
Kanyanta Pythias <kanyantapythias072@gmail.com>
2026-01-25 19:56:31 +0200

    First commit

---
+++ backend/src/test/java/java/salepilot/backend/SalepilotApplicationTests.java
@@ -0,0 +1,13 @@
+package java.salepilot.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalepilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
